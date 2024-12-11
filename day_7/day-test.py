a42768ecd203643a39d2f4afdedfb084cc6f49fb
bobverschoor <bob.verschoor@alliander.com>
2024-12-11 17:56:03 +0100

    day7 try

--- day_7/day-test.py
+++ day_7/day-test.py
@@ -1,10 +1,16 @@
 import unittest
 
+from day_7.day import Day7
 
 
 class day(unittest.TestCase):
     def test_result(self):
-        pass
+        day = Day7()
+        day._inputfilename = "test_input.txt"
+        day.read_input()
+        self.assertEqual(9, len(day.input))
+        day.determine_calibration_result()
+
 
 if __name__ == '__main__':
     unittest.main()
--- day_7/day.py
+++ day_7/day.py
@@ -1,14 +1,54 @@
-class Day:
+import itertools
+
+
+class Day7:
     def __init__(self):
         self._inputfilename = "input.txt"
+        self.input = []
 
     def read_input(self):
         with open(self._inputfilename) as f:
             lines = f.readlines()
         for line in lines:
-            pass
+            line = line.strip()
+            uitkomst, factor = line.split(':')
+            factor = factor.strip()
+            factoren = list(map(int, factor.split(' ')))
+            self.input.append([int(uitkomst), factoren])
+
+    def determine_calibration_result(self):
+        possible_calibrations = []
+        for calc in self.input:
+            uitkomst = calc[0]
+            all_permutations = calculate_possibilities(calc[1])
+            print(all_permutations)
+
+
+def calculate_possibilities(calc):
+    calc_permutations = []
+    for c in calc:
+        if len(calc_permutations) != 0:
+            calc_permutations.append("+")
+            calc_permutations.append(c)
+        else:
+            calc_permutations.append(c)
+    all_permutations = []
+    max_permutations = 2 ** (len(calc) - 1 )
+    for i in range(max_permutations):
+        perm = []
+        for c in range(len(calc_permutations)):
+            if '+' == calc_permutations[c]:
+                perm.append('+')
+                calc_permutations[c] = '*'
+            elif '*' == calc_permutations[c]:
+                perm.append('*')
+                calc_permutations[c] = '+'
+            else:
+                perm.append(calc_permutations[c])
+        all_permutations.append(perm)
+    return all_permutations
 
 
 if __name__ == '__main__':
-    day = Day()
+    day = Day7()
     day.read_input()
