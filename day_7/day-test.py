e1feaa638ceac29a22c05e6381131e47c8add3b2
bobverschoor <bob.verschoor@alliander.com>
2024-12-13 16:47:00 +0100

    day7 end

--- day_7/day-test.py
+++ day_7/day-test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from day_7.day import Day7
+from day_7.day import Day7, calculate_left2right
 
 
 class day(unittest.TestCase):
@@ -9,8 +9,13 @@ class day(unittest.TestCase):
         day._inputfilename = "test_input.txt"
         day.read_input()
         self.assertEqual(9, len(day.input))
-        day.determine_calibration_result()
-
+        self.assertEqual(29, calculate_left2right([10, '+', 19]))
+        self.assertEqual(190, calculate_left2right([10, '*', 19]))
+        self.assertEqual(205, calculate_left2right([10, '*', 19, '+', 15]))
+        self.assertEqual(3749, day.determine_calibration_result())
+        self.assertEqual(1019, calculate_left2right([10, '||', 19]))
+        self.assertEqual(19015, calculate_left2right([10, '*', 19, '||', 15]))
+        self.assertEqual(11387, day.determine_calibration_result(binary=False))
 
 if __name__ == '__main__':
     unittest.main()
--- day_7/day.py
+++ day_7/day.py
@@ -14,31 +14,70 @@ class Day7:
             factoren = list(map(int, factor.split(' ')))
             self.input.append([int(uitkomst), factoren])
 
-    def determine_calibration_result(self):
-        possible_calibrations = []
+    def determine_calibration_result(self, binary = True):
+        totaal = 0
         for calc in self.input:
             uitkomst = calc[0]
-            all_permutations = build_binary_tree(calc[1])
+            if binary:
+                all_permutations = build_binary_tree(calc[1])
+            else:
+                all_permutations = build_ternary_tree(calc[1])
+            if is_correct_outcome(uitkomst, all_permutations):
+                totaal = totaal + uitkomst
+        return totaal
+
+    def determine_calibration_result_part2(self):
+        totaal = 0
+        for calc in self.input:
+            uitkomst = calc[0]
+            all_permutations = build_ternary_tree(calc[1])
+            if is_correct_outcome(uitkomst, all_permutations):
+                totaal = totaal + uitkomst
+        return totaal
 
 
-
-class Node:
+class Node_binary:
     def __init__(self, data, operator = ""):
-        self.plus = None
-        self.multiply = None
+        self.left = None
+        self.right = None
         self.number = data[0]
         self.operator = operator
         if len(data) > 1:
             self.insert(data[1:])
 
     def insert(self, data):
-        self.left = Node(data, "+")
-        self.right = Node(data, "*")
+        self.left = Node_binary(data, "+")
+        self.right = Node_binary(data, "*")
 
 
+class Node_ternary:
+    def __init__(self, data, operator = ""):
+        self.left = None
+        self.right = None
+        self.middle = None
+        self.number = data[0]
+        self.operator = operator
+        if len(data) > 1:
+            self.insert(data[1:])
+
+    def insert(self, data):
+        self.left = Node_ternary(data, "+")
+        self.right = Node_ternary(data, "*")
+        self.middle = Node_ternary(data, "||")
+
+def build_ternary_tree(calc):
+    return Node_ternary(calc)
 
 def build_binary_tree(calc):
-    return Node(calc)
+    return Node_binary(calc)
+
+def is_correct_outcome(uitkomst, permutations):
+    paths = []
+    collect_paths(permutations, [], paths)
+    for path in paths:
+        if calculate_left2right(path) == uitkomst:
+            return True
+    return False
 
 
 def collect_paths(node, path, paths):
@@ -46,7 +85,10 @@ def collect_paths(node, path, paths):
         return
 
     # Append this node to the path
-    path.append(node.data)
+    if node.operator != "":
+        path.append(node.operator)
+    path.append(node.number)
+
 
     # If it's a leaf node, store the path
     if node.left is None and node.right is None:
@@ -55,17 +97,54 @@ def collect_paths(node, path, paths):
         # Otherwise, try both subtrees
         collect_paths(node.left, path, paths)
         collect_paths(node.right, path, paths)
+        if isinstance(node, Node_ternary):
+            collect_paths(node.middle, path, paths)
     # Backtrack: remove the last element from the path
     path.pop()
+    if len(path) > 1:
+        path.pop()
 
 
-def paths(root):
-    paths = []
-    collect_paths(root, [], paths)
-    return paths
+def calculate_left2right(som):
+    uitkomst = 0
+    i = 0
+    while i < len(som):
+        if uitkomst == 0:
+            op1 = som[i]
+            i += 1
+            if som[i] == '+':
+                i += 1
+                op2 = som[i]
+                uitkomst = op1 + op2
+            elif som[i] == '*':
+                i += 1
+                op2 = som[i]
+                uitkomst = op1 * op2
+            elif som[i] == '||':
+                i += 1
+                op2 = som[i]
+                uitkomst = int(str(op1) + str(op2))
+        else:
+            if som[i] == '+':
+                i += 1
+                op2 = som[i]
+                uitkomst = uitkomst + op2
+            elif som[i] == '*':
+                i += 1
+                op2 = som[i]
+                uitkomst = uitkomst * op2
+            elif som[i] == '||':
+                i += 1
+                op2 = som[i]
+                uitkomst = int(str(uitkomst) + str(op2))
 
+        i += 1
+
+    return uitkomst
 
 
 if __name__ == '__main__':
     day = Day7()
     day.read_input()
+    print(day.determine_calibration_result())
+    print(day.determine_calibration_result(binary=False))
