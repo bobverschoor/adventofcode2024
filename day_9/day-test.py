1d02f8ceba2b3b5c1432593dabffa71542f40554
bobverschoor <bob.verschoor@alliander.com>
2024-12-20 16:15:36 +0100

    day9 part 1

--- day_9/day-test.py
+++ day_9/day-test.py
@@ -1,10 +1,20 @@
 import unittest
 
+from day_9.day import Day
 
 
 class day(unittest.TestCase):
     def test_result(self):
-        pass
+        day = Day()
+        day._inputfilename = "test_input.txt"
+        day.read_input()
+        day.extract_disk_map()
+        self.assertEqual([0, 0, '.','.','.',1, 1, 1, '.','.','.',2, '.','.','.', 3, 3,3,'.', 4,4,'.',5,5,5,5,'.',
+                          6,6,6,6,'.',7,7,7,'.',8,8,8,8,9,9], day.extracted_diskmap)
+        day.defragment_disk()
+        self.assertEqual("0099811188827773336446555566", "".join(map(str, day.defragmented_diskmap)))
+        self.assertEqual(1928, day.calculate_checksum())
+
 
 if __name__ == '__main__':
     unittest.main()
--- day_9/day.py
+++ day_9/day.py
@@ -1,14 +1,53 @@
+from itertools import batched
+
+
 class Day:
     def __init__(self):
         self._inputfilename = "input.txt"
+        self.diskmap = ""
+        self.extracted_diskmap = []
+        self.defragmented_diskmap = []
 
     def read_input(self):
         with open(self._inputfilename) as f:
-            lines = f.readlines()
-        for line in lines:
-            pass
+            self.diskmap = f.read()
+
+    def extract_disk_map(self):
+        id = 0
+        if len(self.diskmap) % 2 == 1:
+            self.diskmap = self.diskmap + '0'
+        for fileblock, freespaceblock in batched(self.diskmap, n=2):
+            for i in range(int(fileblock)):
+                self.extracted_diskmap.append(id)
+            for i in range(int(freespaceblock)):
+                self.extracted_diskmap.append('.')
+            id += 1
+
+    def defragment_disk(self):
+        last_item = len(self.extracted_diskmap) - 1
+        for i in range(len(self.extracted_diskmap)):
+            if self.extracted_diskmap[i] == '.':
+                while self.extracted_diskmap[last_item] == '.':
+                    last_item -= 1
+                if last_item > i:
+                    self.defragmented_diskmap.append(self.extracted_diskmap[last_item])
+                    self.extracted_diskmap[last_item] = '.'
+                else:
+                    break
+            else:
+                self.defragmented_diskmap.append(self.extracted_diskmap[i])
+
+    def calculate_checksum(self):
+        checksum = 0
+        for i in range(len(self.defragmented_diskmap)):
+            checksum += i * int(self.defragmented_diskmap[i])
+        return checksum
+
 
 
 if __name__ == '__main__':
     day = Day()
     day.read_input()
+    day.extract_disk_map()
+    day.defragment_disk()
+    print(day.calculate_checksum())
