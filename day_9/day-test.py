d7daadf574b718ffda96933b5a6df8e9a6d2ecab
bob <bob.verschoor@alliander.com>
2024-12-21 17:05:16 +0100

    day 7 part 2 end

--- day_9/day-test.py
+++ day_9/day-test.py
@@ -14,7 +14,49 @@ class day(unittest.TestCase):
         day.defragment_disk()
         self.assertEqual("0099811188827773336446555566", "".join(map(str, day.defragmented_diskmap)))
         self.assertEqual(1928, day.calculate_checksum())
+        day.extract_disk_map()
+        self.assertEqual((2, 9, 39),
+                         day.get_block_from_end(41))
+        self.assertEqual((4, 8, 35),
+                         day.get_block_from_end(39))
+        self.assertEqual((3, 7, 31),
+                         day.get_block_from_end(35))
+        self.assertEqual((1, 2, 10),
+                         day.get_block_from_end(11))
+        self.assertEqual((2, 0, -1),
+                         day.get_block_from_end(1))
+        self.assertEqual(2, day.get_start_of_empty_space_with_length(3, 42))
+        self.assertEqual(-1, day.get_start_of_empty_space_with_length(4, 42))
+        day.extracted_diskmap[2] = 9
+        day.extracted_diskmap[3] = 9
+        day.extracted_diskmap[40] = '.'
+        day.extracted_diskmap[41] = '.'
+        self.assertEqual(8, day.get_start_of_empty_space_with_length(3, 39))
+        day.fit_block_in_extracted_map(4, 8, 35)
+        self.assertEqual("0099.111...2...333.44.5555.6666.777.8888..", "".join(map(str, day.extracted_diskmap)))
+        day.fit_block_in_extracted_map(3, 7, 35)
+        day.extracted_diskmap[32] = '.'
+        day.extracted_diskmap[33] = '.'
+        day.extracted_diskmap[34] = '.'
+        self.assertEqual("0099.1117772...333.44.5555.6666.....8888..", "".join(map(str, day.extracted_diskmap)))
+        day.fit_block_in_extracted_map(2, 4, 20)
+        day.extracted_diskmap[19] = '.'
+        day.extracted_diskmap[20] = '.'
+        self.assertEqual("0099.111777244.333....5555.6666.....8888..", "".join(map(str, day.extracted_diskmap)))
+        day.fit_block_in_extracted_map(1, 2, 12)
+        day.extracted_diskmap[11] = '.'
+        self.assertEqual("00992111777.44.333....5555.6666.....8888..", "".join(map(str, day.extracted_diskmap)))
+        day.extract_disk_map()
+        day.defragment_disk2()
+        self.assertEqual("00992111777.44.333....5555.6666.....8888..", "".join(map(str, day.extracted_diskmap)))
+        self.assertEqual(2858, day.calculate_checksum())
 
+    def test_part2(self):
+        day = Day()
+        day.diskmap = "1313165"
+        day.extract_disk_map()
+        day.defragment_disk2()
+        self.assertEqual(169, day.calculate_checksum())
 
 if __name__ == '__main__':
     unittest.main()
--- day_9/day.py
+++ day_9/day.py
@@ -13,6 +13,7 @@ class Day:
             self.diskmap = f.read()
 
     def extract_disk_map(self):
+        self.extracted_diskmap = []
         id = 0
         if len(self.diskmap) % 2 == 1:
             self.diskmap = self.diskmap + '0'
@@ -37,10 +38,65 @@ class Day:
             else:
                 self.defragmented_diskmap.append(self.extracted_diskmap[i])
 
+    def defragment_disk2(self):
+        original_extracted_diskmap = self.extracted_diskmap.copy()
+        start_next_block = len(original_extracted_diskmap) - 1
+        while start_next_block >= 0:
+            length_block, id, start_next_block  = self.get_block_from_end(start_next_block)
+            if self.fit_block_in_extracted_map(length_block, id, start_next_block + 1) and length_block > 0:
+                for i in range(start_next_block + 1, start_next_block + 1 + length_block):
+                    self.extracted_diskmap[i] = '.'
+        self.defragmented_diskmap = self.extracted_diskmap
+
+    def fit_block_in_extracted_map(self, length_block, id, end_fit):
+        start_of_fitting_block = self.get_start_of_empty_space_with_length(length_block, end_fit)
+        if start_of_fitting_block >= 0:
+            for i in range(length_block):
+                self.extracted_diskmap[start_of_fitting_block + i] = id
+            return True
+        else:
+            return False
+
+    def get_start_of_empty_space_with_length(self, length_block, end_fit):
+        free_space_length = 0
+        start_of_block = -1
+        for i in range(end_fit):
+            if self.extracted_diskmap[i] == '.':
+                if start_of_block == -1:
+                    start_of_block = i
+                free_space_length += 1
+            else:
+                if free_space_length >= length_block:
+                    return start_of_block
+                else:
+                    free_space_length = 0
+                    start_of_block = -1
+        if free_space_length >= length_block:
+            return start_of_block
+        return -1
+
+
+    def get_block_from_end(self, item_nr):
+        if item_nr > 0:
+            for i in range(item_nr, 0, -1):
+                while self.extracted_diskmap[i] == '.':
+                    i -= 1
+                id = self.extracted_diskmap[i]
+                start_block = i
+                length = 0
+                if start_block >= 0:
+                    while self.extracted_diskmap[start_block] == id:
+                        start_block -= 1
+                        length += 1
+                return length, id, start_block
+        return -1, -1, -1
+
+
     def calculate_checksum(self):
         checksum = 0
         for i in range(len(self.defragmented_diskmap)):
-            checksum += i * int(self.defragmented_diskmap[i])
+            if self.defragmented_diskmap[i] != '.':
+               checksum += i * int(self.defragmented_diskmap[i])
         return checksum
 
 
@@ -50,4 +106,7 @@ if __name__ == '__main__':
     day.read_input()
     day.extract_disk_map()
     day.defragment_disk()
+    print(day.calculate_checksum())
+    day.extract_disk_map()
+    day.defragment_disk2()
     print(day.calculate_checksum())
