3c52d55a2fde4e0f97f39a9dcd36b5fd2362dd36
bob <bob.verschoor@alliander.com>
2024-12-27 10:50:25 +0100

    day 10 part 2 end

--- day_10/day-test.py
+++ day_10/day-test.py
@@ -1,10 +1,72 @@
 import unittest
 
+from day_10.day import Day
 
 
 class day(unittest.TestCase):
-    def test_result(self):
-        pass
+    def test_result_1(self):
+        day = Day()
+        day._input_filename = "test_input.txt"
+        day.read_input()
+        self.assertEqual(7, len(day.input))
+        self.assertEqual(['.','.','.','0','.','.','.'], day.input[0])
+        day.walk_valid_moves(0,0, day.input)
+        self.assertEqual(0, day.number_of_trailheads)
+        day.walk_valid_moves(3,0, day.input)
+        self.assertEqual(2, day.number_of_trailheads)
+
+    def test_result_2(self):
+        day = Day()
+        day._input_filename = "test_input2.txt"
+        day.read_input()
+        day.walk_valid_moves(3,0, day.input)
+        self.assertEqual(4, day.number_of_trailheads)
+
+    def test_result_3(self):
+        day = Day()
+        day._input_filename = "test_input3.txt"
+        day.read_input()
+        day.get_tailheads()
+        self.assertEqual(3, day.number_of_trailheads)
+
+    def test_result_4(self):
+        day = Day()
+        day._input_filename = "test_input4.txt"
+        day.read_input()
+        day.get_tailheads()
+        self.assertEqual(36, day.number_of_trailheads)
+
+    def test_result_5(self):
+        day = Day()
+        day._input_filename = "test_input5.txt"
+        day.read_input()
+        day.distinct = True
+        day.get_tailheads()
+        self.assertEqual(3, day.number_of_trailheads)
+
+    def test_result_6(self):
+        day = Day()
+        day._input_filename = "test_input6.txt"
+        day.read_input()
+        day.distinct = True
+        day.get_tailheads()
+        self.assertEqual(13, day.number_of_trailheads)
+
+    def test_result_7(self):
+        day = Day()
+        day._input_filename = "test_input7.txt"
+        day.read_input()
+        day.distinct = True
+        day.get_tailheads()
+        self.assertEqual(227, day.number_of_trailheads)
+
+    def test_result_8(self):
+        day = Day()
+        day._input_filename = "test_input8.txt"
+        day.read_input()
+        day.distinct = True
+        day.get_tailheads()
+        self.assertEqual(81, day.number_of_trailheads)
 
 if __name__ == '__main__':
     unittest.main()
--- day_10/day.py
+++ day_10/day.py
@@ -1,14 +1,74 @@
+from copy import deepcopy
+
 class Day:
     def __init__(self):
         self._input_filename = "input.txt"
+        self.input = []
+        self.number_of_trailheads = 0
+        self.reached_tops = []
+        self.distinct = False
 
     def read_input(self):
         with open(self._input_filename) as f:
             lines = f.readlines()
         for line in lines:
-            pass
+            line = line.strip()
+            self.input.append(list(line))
+
+    def get_tailheads(self):
+        for y in range(len(self.input)):
+            for x in range(len(self.input[y])):
+                if self.input[y][x] == '0':
+                    grid = deepcopy(self.input)
+                    self.reached_tops = []
+                    self.walk_valid_moves(x, y, grid)
+
+
+    def walk_valid_moves(self, x, y, grid):
+        if grid[y][x] != '.':
+            value = int(grid[y][x])
+        else:
+            value = grid[y][x]
+        if value == 9:
+            if self.distinct:
+                self.number_of_trailheads += 1
+            else:
+                if not top_already_reached(x, y, self.reached_tops):
+                    self.number_of_trailheads += 1
+                    self.reached_tops.append((x, y))
+        else:
+            if is_valid_int(x, y + 1, value, grid):
+                self.walk_valid_moves(x, y + 1, grid)
+            if is_valid_int(x + 1, y, value, grid):
+                self.walk_valid_moves(x + 1, y, grid)
+            if is_valid_int(x, y - 1, value, grid):
+                self.walk_valid_moves(x, y - 1, grid)
+            if is_valid_int(x - 1, y, value, grid):
+                self.walk_valid_moves(x - 1, y, grid)
+
+def is_valid_int(x, y, value, grid):
+    if 0 <= y < len(grid) and 0 <= x < len(grid[y]):
+        if grid[y][x] != '.' and int(grid[y][x]) == value + 1:
+            return True
+    return False
+
+
+def top_already_reached(x, y, tops):
+    for top in tops:
+        reached_x, reached_y = top
+        if reached_x == x and reached_y == y:
+            return True
+    return False
 
 
 if __name__ == '__main__':
     day = Day()
     day.read_input()
+    day.get_tailheads()
+    print(day.number_of_trailheads)
+
+    day2 = Day()
+    day2.read_input()
+    day2.distinct = True
+    day2.get_tailheads()
+    print(day2.number_of_trailheads)
